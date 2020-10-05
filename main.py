7e84476f5679af9e76e378c93cc701fdf034030b
Daniel Paxton <dp041367@sdale.org>
2020-10-05 17:38:58 +0000

    Stuff

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 '''
 
 
@@ -6,11 +7,9 @@ Game
 
 
 '''
-from PIL import Image
 print("Metro 2033")
-print("The untold stories of Khan")
 
-play = input("Press x to play>> ")
+play = input("Press x to play >> ")
 
 
 start = False
@@ -23,7 +22,6 @@ if play == ("x"):
 if (start):
   print("I was in the metro again, I was lost, I need to find the nearest station")
 
-metroMap1 = ["Follow the rail tracks", "Go above ground", "Swim through the sewers" ]
 
 
 #Tells which map your are in
@@ -32,20 +30,41 @@ map2 = False
 map3 = False
 
 
+# Health points
+health = int(10)
+mutant_1 = int(2)
 
-
-
-
+# User picks direction
 userInput1 = input("Follow the rail tracks, Go above ground, Swim through the sewers >>")
+mapPick = ["Follow the rail tracks", "Go above ground", "Swim through the sewers" ]
 
-def metro(metroMap1):
-  if userInput1 == (metroMap1[0]):
+def metro(mapPick, health):
+  if userInput1 == (mapPick[0]):
+    
     print("\n I followed the trail, the only thing that you can hear are my footsteps. There is a legend that if you listen close enough you can hear the cries of souls telling you to go back. Up ahead you hear a bell, that is the same sound you hear if a station was opening a gate. I travelled further to figure out it is a live station with people! Hopefully they would let me in.")
-    map1 = True
-    map = Image.open('map1.jpg')
-    map.show()
-metro(metroMap1)
+    
+    #Tells whether mutant_1 is alive. True = Dead
+    mutant_1 = False
 
+
+    mutant_1_attack = random.randint(1,2)
+    print("\n A dark figure approaches you. It is a mutant creature, 2 HP, 2 Dmg")
+    print("Mutant Damage:", mutant_1_attack)
+    health -= mutant_1_attack
+    
+  
+    
+    # Map1 = True
+    if (mutant_1):
+      continueStation = input("\n Do I continue to the next station y/n >> ")
+      if continueStation == ("y"):
+        print("When I arrived at the station they noticed my uniform. They immediately recognized which faction I am from. These were the guards of the Red Line faction. This meant they were armed. He threatend me to leave.")
+    
+metro(mapPick)
+
+def metro(mapSewer):
+  if userInput1 == mapPick[1]:
+    print("Test")
 
 
 
